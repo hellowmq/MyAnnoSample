985bac0370fe0ed0faea1456e99b5d6824e283a4
ifans.wen <ifans.wen@corp.to8to.com>
2020-06-23 16:27:38 +0800

    【ADD】 add alpha support

--- app/src/main/java/com/wenmq/annosample/MainActivity.java
+++ app/src/main/java/com/wenmq/annosample/MainActivity.java
@@ -1,9 +1,14 @@
 package com.wenmq.annosample;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.TextView;
 
+import com.alibaba.android.alpha.OnProjectExecuteListener;
 import com.wenmq.anno.NewIntent;
+import com.wenmq.annosample.alpha.ConfigTest;
+import com.wenmq.annosample.alpha.MyLog;
 import com.wenmq.annotationsample.ANavigator;
 
 @NewIntent(path = RouterPath.Activity.Path_MainActivity)
@@ -22,7 +27,36 @@ public class MainActivity extends BaseActivity {
     private void initView() {
         TextView textView = findViewById(R.id.textView);
         textView.setOnClickListener(v -> startActivity(ANavigator.getIntentByPath(MainActivity.this, RouterPath.Activity.Path_SecondActivity)));
-
+        addTestTask(textView);
 
     }
+
+
+    private void addTestTask(final TextView text) {
+        ConfigTest test = new ConfigTest(getApplicationContext());
+        test.setOnProjectExecuteListener(new OnProjectExecuteListener() {
+            @Override
+            public void onProjectStart() {
+
+            }
+
+            @Override
+            public void onTaskFinish(String taskName) {
+
+            }
+
+            @Override
+            public void onProjectFinish() {
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        text.setText(MyLog.getLogString());
+                    }
+                });
+            }
+        });
+        test.start();
+    }
+
+
 }
---
+++ app/src/main/java/com/wenmq/annosample/alpha/TaskFactory.java
@@ -0,0 +1,19 @@
+package com.wenmq.annosample.alpha;
+
+/**
+ * @author ifans.wen
+ * @date 2020/6/23
+ * @description
+ */
+public class TaskFactory {
+
+
+
+
+
+
+
+
+
+
+}
